8354227d7ed2c63329079d9f7dcdbb16fac85bfe
gptestudent <gptestudent@gptestudent.com>
2017-10-25 11:32:35 +0200

    added load generator method added DATA_FOLDER env variable tuned hello world output

--- src/main/java/net/metja/csv/APIController.java
+++ src/main/java/net/metja/csv/APIController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
 import java.io.FileReader;
 import java.util.List;
 
@@ -23,13 +24,24 @@ import java.util.List;
 @RestController
 public class APIController {
 
+    private final String DATA_FOLDER_KEY = "DATA_FOLDER";
     private Logger logger = LoggerFactory.getLogger(APIController.class);
+    private String dataFolder = null;
+
+    public APIController() {
+        if(System.getenv(this.DATA_FOLDER_KEY) != null) {
+            this.dataFolder = System.getenv(this.DATA_FOLDER_KEY);
+        } else {
+            this.logger.warn("Env variable "+this.DATA_FOLDER_KEY+" not set! Setting data folder as /.");
+            this.dataFolder = "/";
+        }
+    }
 
     @RequestMapping(value = "/csv/v1/read/{filename}", produces= MediaType.APPLICATION_JSON_VALUE, method= RequestMethod.GET)
     public ResponseEntity<List<PersonPTO>> readPersonCSV(@PathVariable(name = "filename")String filename) {
         logger.info("Filename: "+filename);
         try {
-            CSVReader csvReader = new CSVReader(new FileReader(filename), ',', '"', 0);
+            CSVReader csvReader = new CSVReader(new FileReader(new File(this.dataFolder, filename)), ',', '"', 0);
             CsvToBean<PersonPTO> csv = new CsvToBean<>();
             List<PersonPTO> persons = csv.parse(this.setColumnMapping(), csvReader);
             return new ResponseEntity<List<PersonPTO>>(persons, HttpStatus.OK);
@@ -42,7 +54,7 @@ public class APIController {
     public ResponseEntity<PersonPTO> readPersonCSVWithID(@PathVariable(name = "filename")String filename, @PathVariable(name = "id")int id) {
         logger.info("Filename: "+filename);
         try {
-            CSVReader csvReader = new CSVReader(new FileReader(filename), ',', '"', 0);
+            CSVReader csvReader = new CSVReader(new FileReader(new File(this.dataFolder, filename)), ',', '"', 0);
             CsvToBean<PersonPTO> csv = new CsvToBean<>();
             List<PersonPTO> persons = csv.parse(this.setColumnMapping(), csvReader);
             for(PersonPTO person: persons) {
@@ -64,9 +76,35 @@ public class APIController {
         return strategy;
     }
 
+    /**
+     * Hello world for connectivity / deployment testing
+     *
+     * @param name
+     * @return
+     */
     @RequestMapping(value="/csv/v1/hello/{name}", produces= MediaType.APPLICATION_JSON_VALUE, method= RequestMethod.GET)
     public ResponseEntity<String> hello(@PathVariable(value="name", required=true)String name) {
-        return new ResponseEntity("{\"Hello\": \""+name+"!\"}", HttpStatus.OK);
+        return new ResponseEntity("{ \"hello\":\""+name+"!\" }", HttpStatus.OK);
+    }
+
+    /**
+     * Generates load via a busy loop for a given amount of seconds
+     *
+     * @param time time in seconds
+     * @return
+     */
+    @RequestMapping(value="/csv/v1/load/{time}", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
+    public ResponseEntity<String> generateLoad(@PathVariable(value="time", required = true)int time) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                long begin = System.currentTimeMillis();
+                while(System.currentTimeMillis() < (begin+(time*1000))) {
+                    int result = 1+1;
+                }
+            }
+        }).start();
+        return new ResponseEntity<String>("{\"load\": \""+time+"s\"}", HttpStatus.OK);
     }
 
 }
