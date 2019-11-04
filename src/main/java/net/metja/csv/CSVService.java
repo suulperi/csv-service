f743b2c3056edd6d9ed22b2be6a2c66d56cd13d1
jmetso <jmetso@redhat.com>
2019-11-04 14:29:50 +0200

    code cleanup

--- src/main/java/net/metja/csv/CSVService.java
+++ src/main/java/net/metja/csv/CSVService.java
@@ -1,12 +1,7 @@
 package net.metja.csv;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-
-import java.util.Arrays;
 
 /**
  * Created by Janne Metso on 2017-07-13.
